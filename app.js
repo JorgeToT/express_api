04d5ca02ae0ac3e17a296671ebac1d2cb189c889
Jorge Aguilar <jorge.agvel@gmail.com>
2022-04-20 22:20:52 -0500

    Agregando metodos GET, POST, PUT, DEL

--- app.js
+++ app.js
@@ -7,14 +7,40 @@ app.use(express.json()); // Indicamos que usaremos JSON
 const port = 3000;
 
 // HTTP METHODS
-app.get("/vl/explorers", (req, res) => {
-  console.log("Api Explorers GET ALL requests" + new Date());
+app.get("/v1/explorers", (req, res) => {
+  console.log("API Explorers GET ALL requests " + new Date());
   const explorer1 = { id: 1, name: "Jorge 1" };
   const explorer2 = { id: 2, name: "Jorge 2" };
   const explorer3 = { id: 3, name: "Jorge 3" };
   const explorer4 = { id: 4, name: "Jorge 4" };
   const explorers = [explorer1, explorer2, explorer3, explorer4];
   res.status(200).json(explorers);
+});
+
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log("API Explorers GET request " + new Date());
+  console.log("Getting explorer with id " + req.params.id);
+  const explorer = { id: 1, name: "Carlo" };
+  res.status(200).json(explorer);
+});
+
+app.post("/v1/explorers", (req, res) => {
+  console.log("API Explorers POST request " + new Date());
+  const requestBody = req.body; // Parámetros de un cliente
+  res.status(201).json({ message: "Created" });
+});
+
+app.put("/v1/explorers/:id", (req, res) => {
+  console.log("API Explorers PUT request " + new Date());
+  console.log("Update explorer with id " + req.params.id);
+  const requestBody = req.body; // Parámetros de un cliente
+  res.status(200).json({ message: "Updated!" });
+});
+
+app.delete("/v1/explorers/:id", (req, res) => {
+  console.log("API Explorers DELETE request " + new Date());
+  console.log("Delete explorer with id " + req.params.id);
+  res.status(200).json({ message: "Deleted!" });
 });
 
 // Con esto inicializamos esta app
